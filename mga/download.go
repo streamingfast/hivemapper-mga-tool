dc38246e3d55ff6afc7db79ddd2c03e09d84af15
HariHive <haripriya@hivemapper.com>
2023-06-22 16:23:05 -0700

    Testing download file

--- mga/download.go
+++ mga/download.go
@@ -9,29 +9,30 @@ import (
 )
 
 func Download(mgaServiceURL string) (mgaData []byte, hash string, err error) {
-	resp, err := http.Get(mgaServiceURL)
-	if err != nil {
-		err = fmt.Errorf("downloading MGA data: %w", err)
-		return
-	}
+	fmt.Println("downloading MGA data from:", mgaServiceURL)
+	// resp, err := http.Get(mgaServiceURL)
+	// if err != nil {
+	// 	err = fmt.Errorf("downloading MGA data: %w", err)
+	// 	return
+	// }
 
-	if resp.StatusCode != http.StatusOK {
-		err = fmt.Errorf("downloading MGA failed with cide: %d", resp.StatusCode)
-		return
-	}
+	// if resp.StatusCode != http.StatusOK {
+	// 	err = fmt.Errorf("downloading MGA failed with cide: %d", resp.StatusCode)
+	// 	return
+	// }
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		err = fmt.Errorf("reading MGA data: %w", err)
-		return
-	}
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	err = fmt.Errorf("reading MGA data: %w", err)
+	// 	return
+	// }
 
-	fmt.Println("read body:", len(body))
-	mgaData = body
+	// fmt.Println("read body:", len(body))
+	// mgaData = body
 
-	h := md5.New()
-	h.Write(body)
-	hash = hex.EncodeToString(h.Sum(nil))
+	// h := md5.New()
+	// h.Write(body)
+	// hash = hex.EncodeToString(h.Sum(nil))
 
 	return
 }
